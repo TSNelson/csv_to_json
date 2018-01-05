12dd53da59414abceef528ed72f68d56ffb21b5e
Tor Nelson <bb0c79a8@opayq.com>
2018-01-05 07:51:29 +0000

    completed alternate version

--- convertFile.js
+++ convertFile.js
@@ -2,6 +2,7 @@
 
 const fs = require("fs");
 const path = require("path");
+const csv = require("csvtojson");
 
 
 function convert(file) {
@@ -12,23 +13,39 @@ function convert(file) {
             console.error(error);
             throw error;
         } else {
-            // convert file with json
-            const jsonData = ""; // add call to library
-            const outputFile = path.join(__dirname, "customer-data.json");
-            fs.writeFile(outputFile,
-                         jsonData, 
-                         "utf-8",
-                         (error) => {
-                             if (error) {
-                                 console.log("error on writing file " + outputFile);
-                                 console.error(error);
-                                 throw error;
-                             } else {
-                                 console.log("File conversion complete.");
-                             }
-                         });
+            dataToObjects(data);
         }
     });
+}
+
+function dataToObjects(data) {
+    let output = [];
+    csv().fromString(data)
+         .on('json', (rowObj) => {
+             //console.log(rowObj);
+             output.push(rowObj);
+         })
+         .on('done', () => {
+             console.log("File converted to JSON using csvtojson.");
+             writeToFile(output);
+         });
+}
+
+function writeToFile(obj) {
+    const jsonData = JSON.stringify(obj, null, 2);
+    const outputFile = path.join(__dirname, "customer-data.json");
+    fs.writeFile(outputFile,
+                 jsonData, 
+                 "utf-8",
+                 (error) => {
+                    if (error) {
+                        console.log("error on writing file " + outputFile);
+                        console.error(error);
+                        throw error;
+                    } else {
+                        console.log("File conversion complete.");
+                    }
+                 });
 }
 
 convert(process.argv[2]);
